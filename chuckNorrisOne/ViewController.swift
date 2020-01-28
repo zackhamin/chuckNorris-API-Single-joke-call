73e48ee9252d1e8b0d49f54fcbd474466f1fb7d2
Zack <ishaqamincoop@icloud.com>
2020-01-28 11:24:52 +0000

    Initial Commit

---
+++ chuckNorrisOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chuckNorrisOne
+//
+//  Created by Ishaq Amin on 28/01/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
